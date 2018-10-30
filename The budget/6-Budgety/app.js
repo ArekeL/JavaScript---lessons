6fee0295c3aa45e4a316c59f303e2c4936e83788
Arek eL <alechnik@gmail.com>
2018-10-30 07:18:51 +0100

    Working on setupEventListeners

--- The budget/6-Budgety/app.js
+++ The budget/6-Budgety/app.js
@@ -33,13 +33,26 @@ var UIController = (function(){
 // GLOBAL CONTROLLER
 var controller = (function(budgetCTRL, UICtrl){
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function () {
+
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+
+       if(event.keyCode === 13 || event.which === 13) {
+         ctrlAddItem();
+       }
+    });
+  };
+
+
 
   var ctrlAddItem = function() {
-    // 1. Get the field input data
 
+    // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
 
     // 2. Add the item to the bidget CONTROLLER
 
@@ -51,21 +64,13 @@ var controller = (function(budgetCTRL, UICtrl){
 
   };
 
-
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event){
-
-     if(event.keyCode === 13 || event.which === 13) {
-       ctrlAddItem();
-     }
-
-  });
-
-
-
-
-
-
+  return {
+    init: function () {
+      console.log('Application has started.');
+      setupEventListeners();
+    }
+  };
 
 })(budgetController, UIController);
+
+controller.init();
