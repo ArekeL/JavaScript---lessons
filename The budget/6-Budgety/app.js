1f707481e717cccb5f9b2de4a2c0fc2f75288f7f
Arek eL <alechnik@gmail.com>
2018-10-31 05:58:49 +0100

    Create-ID/Item(inc,exp)/PushData/ReturnElement.Not finish yet

--- The budget/6-Budgety/app.js
+++ The budget/6-Budgety/app.js
@@ -24,6 +24,40 @@ var budgetController = (function(){
     }
   };
 
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // ID = last ID +1
+      // Create ne ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+
+      // Create new Item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      }else if (type === 'inc') {
+         newItem = new Income(ID, des, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+      //Return the new element
+      return newItem;
+    },
+
+    testing: function() {
+
+      console.log(data);
+
+    }
+
+  };
+
 })();
 
 
@@ -48,6 +82,10 @@ var UIController = (function(){
 
     getDOMstrings: function() {
       return DOMstrings;
+    },
+
+    testing: function() {
+      console.log(data);
     }
 
   };
@@ -75,10 +113,13 @@ var controller = (function(budgetCTRL, UICtrl){
 
   var ctrlAddItem = function() {
 
+    var input, newItem;
+
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
     // 2. Add the item to the bidget CONTROLLER
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
 
