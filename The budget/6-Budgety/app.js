7bb9c9390cdaf26aaa355cc6a12a8d6eb8555d3c
Arek eL <alechnik@gmail.com>
2018-11-08 07:12:54 +0100

    clearFields but stil some bug

--- The budget/6-Budgety/app.js
+++ The budget/6-Budgety/app.js
@@ -105,6 +105,21 @@ var UIController = (function(){
 
     },
 
+    clearFields: function(){
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value == "";
+      });
+
+      fieldsArr[0].focus();
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -113,7 +128,7 @@ var UIController = (function(){
 })();
 
 // GLOBAL CONTROLLER
-var controller = (function(budgetCTRL, UICtrl){
+var controller = (function(budgetCtrl, UICtrl){
 
   var setupEventListeners = function () {
 
@@ -143,6 +158,9 @@ var controller = (function(budgetCTRL, UICtrl){
 
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+    // 4. Clear fields
+    UICtrl.clearFields();
+
     // 4. Calculate the budget
 
     // 5. Display the budget on the
