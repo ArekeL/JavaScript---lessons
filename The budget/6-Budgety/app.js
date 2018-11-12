35acdd7c8fbbb744bcb6b9fb828b923212e6e1e4
Arek eL <alechnik@gmail.com>
2018-11-12 16:42:33 +0100

    deleting the item from the UI

--- The budget/6-Budgety/app.js
+++ The budget/6-Budgety/app.js
@@ -60,6 +60,26 @@ var budgetController = (function(){
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+      // id = 3
+      //data.allItems[type][id];
+      //ids = [1 2 4 6 8]
+      //index = 3
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+
+    },
+
     calculateBudget: function() {
 
       // Calculate total income and expenses
@@ -244,10 +264,10 @@ var controller = (function(budgetCtrl, UICtrl){
 
       //inc-1
       splitID = itemID.split('-');
-      type = splitID(0);
-      ID = splitID(1);
+      type = splitID[0];
+      ID = parseInt(splitID[1]);
       // 1. delete the item from the data structure
-
+      budgetCtrl.deleteItem(type, ID);
       //2. delete the item from the UI
 
       //3. Update and show yhe new budget
