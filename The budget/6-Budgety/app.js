82b94a4f97324cde62814f005f732a43695872ca
Arek eL <alechnik@gmail.com>
2018-11-13 06:43:51 +0100

    displayPercentages function/ Update the UI with percentages some bug

--- The budget/6-Budgety/app.js
+++ The budget/6-Budgety/app.js
@@ -172,7 +172,8 @@ var UIController = (function(){
       incomeLabel: '.budget__income--value',
       expensesLabel: '.budget__expenses--value',
       percentageLabel: '.budget__expenses--percentage',
-      container: '.container'
+      container: '.container',
+      expensesPercLabel: 'item__percentage'
   };
 
   return {
@@ -245,6 +246,24 @@ var UIController = (function(){
 
     },
 
+    displayPercentages: function(percentages) {
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+                  current.textContent =  percentages[index] + ' %';
+        }else {
+                  current.textContent =  '---';
+        }
+      });
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -287,8 +306,8 @@ var controller = (function(budgetCtrl, UICtrl){
     budgetCtrl.calculatePercentages();
     // Read percentages frome the budget controller
     var percentages = budgetCtrl.getPercentages();
-    // Update the UI with the ner percentages
-    console.log(percentages);
+    // Update the UI with the new percentages
+    UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function() {
